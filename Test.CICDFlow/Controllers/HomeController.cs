f7b1cc09852c18948deff3fd0063f0bdcf2c9637
CoolBTech <90653293+CoolBTech@users.noreply.github.com>
2021-09-13 16:11:41 -0500

    test proj

---
+++ Test.CICDFlow/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.CICDFlow.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+           return Ok("testcicd");
+        }
+    }
+}
